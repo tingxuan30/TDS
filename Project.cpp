c24f6524265bc1c96152c927a18c7347a924730f
LOY YU XUAN <1221208223@student.mmu.edu.my>
2025-06-01 02:11:17 +0800

    Declare used library & file path 1. iostream (for io) 2. iomanip (for output formatting) 3. fstream (for file io) 4. string (for string data) 5. file path for txt.file about admin and member

---
+++ Project.cpp
@@ -0,0 +1,12 @@
+// Library used
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+using namespace std;
+
+// File paths
+const string MEMBERS_FILE = "member.txt";
+const string MEMBERS_ID_FILE = "member_id.txt";
+const string ADMINS_FILE = "admin.txt";
+const string ADMINS_ID_FILE = "admin_id.txt";
